6ef370e1be36ae6262155d7ffc8765a9fee1f6c0
CihanErenler <cihanerenler@outlook.com>
2021-11-18 23:31:47 +0200

    switching between nav items added, animations added

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -1,10 +1,93 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useGlobalContext } from "../context";
+import sublinks from "../data";
 
 function Submenu() {
+  const { location, isSubmenuOpen, dimentions, setDimentions } =
+    useGlobalContext();
+  const arrowRef = useRef(null);
+  const innerContainer = useRef(null);
+  const container = useRef(null);
+
+  useEffect(() => {
+    arrowRef.current.style.left = location.location + "px";
+
+    if (location.index > -1) {
+      const width = sublinks[location.index].width;
+      const height = sublinks[location.index].height;
+      setDimentions({ height, width });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  const returnClass = (index) => {
+    if (location.index === -1) return "";
+    if (index === location.index) return "";
+    if (index > location.index) return "right";
+    return "left";
+  };
+
+  const returnDimentions = (d) => {
+    if (d === 480) return 50;
+    if (d === 450) return 65;
+    if (d === 430) return 75;
+  };
+
   return (
-    <div>
-      <h1>This is submenu</h1>
-    </div>
+    <React.Fragment>
+      <div
+        className={` ${isSubmenuOpen ? "sub-arrow show" : "sub-arrow"}`}
+        ref={arrowRef}
+      ></div>
+
+      <section
+        className={` ${
+          isSubmenuOpen ? "submenu-container show" : "submenu-container"
+        }`}
+        ref={container}
+        style={{
+          height: `${dimentions.height}px`,
+          width: `${dimentions.width}px`,
+        }}
+      >
+        <div className="submenu-inner">
+          {sublinks.map((link, index) => {
+            const { page, links } = link;
+            return (
+              <div
+                key={index}
+                className={`${
+                  location.page === page
+                    ? "submenu-inner-content visible"
+                    : "submenu-inner-content"
+                } ${returnClass(index)}`}
+                ref={innerContainer}
+                style={{
+                  marginLeft: `${returnDimentions(dimentions.width)}px`,
+                }}
+              >
+                <h2 className="submenu-title">{page}</h2>
+                <div className={page}>
+                  {links.map((link, index) => {
+                    const { label, url, icon } = link;
+                    return (
+                      <a
+                        className="sub-link flex align-center"
+                        key={index}
+                        href={url}
+                      >
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </React.Fragment>
   );
 }
 
--- src/context.js
+++ src/context.js
@@ -1,9 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const [isSubmenuOpen, setIsOpenSubmenuOpen] = useState(false);
+  const [showInner, setShowInner] = useState(false);
+  const [dimentions, setDimentions] = useState({ height: 0, width: 0 });
+  const [location, setLocation] = useState({
+    index: -1,
+    page: "",
+    location: "",
+  });
+
+  const openSubmenu = () => {
+    setIsOpenSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsOpenSubmenuOpen(false);
+    setLocation({ ...location, index: -1 });
+  };
+  const makeVisible = () => {
+    setShowInner(true);
+  };
+  const makeNotVisible = () => {
+    setShowInner(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        location,
+        setLocation,
+        openSubmenu,
+        closeSubmenu,
+        isSubmenuOpen,
+        makeVisible,
+        makeNotVisible,
+        setDimentions,
+        dimentions,
+        showInner,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
